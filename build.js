3b8ba300dc2ac428d35726d02f51975e6306f948
Jaalah Ramos <jaalah.ramos@gmail.com>
2023-04-26 14:13:25 -0400

    Saving

--- build.js
+++ build.js
@@ -1,55 +1,54 @@
 const StyleDictionaryPackage = require('style-dictionary');
 
 function getStyleDictionaryConfig(brand, platform) {
-    return {
-        source: [
-            'tokens/alias/**/*.json',
-            'tokens/components/**/*.json',
-            `tokens/brands/${brand}/*.json`,
-            'tokens/globals/**/*.json',
-            `tokens/platforms/${platform}/*.json`
-        ],
-        platforms: {
-            'web/js': {
-                transformGroup: 'tokens-js',
-                buildPath: `dist/web/${brand}/`,
-                files: [
-                    {
-                        destination: 'tokens.es6.js',
-                        format: 'javascript/es6',
-                        filter: 'excludeBrand'
-                    }
-                ]
-            },
-            'web/json': {
-                transformGroup: 'tokens-json',
-                buildPath: `dist/web/${brand}/`,
-                files: [
-                    {
-                        destination: 'tokens.json',
-                        format: 'json/flat',
-                        filter: 'excludeBrand'
-                    }
-                ]
-            },
-            'web/scss': {
-                transformGroup: 'tokens-scss',
-                buildPath: `dist/web/${brand}/`,
-                files: [
-                    {
-                        destination: 'tokens.scss',
-                        format: 'scss/variables',
-                        filter: 'excludeBrand',
-                        options: {
-                            outputReferences: false
-                        }
-                    }
-                ]
+  return {
+    source: [
+      'tokens/alias/**/*.json',
+      'tokens/components/**/*.json',
+      `tokens/brands/${brand}/*.json`,
+      'tokens/globals/**/*.json',
+      `tokens/platforms/${platform}/*.json`
+    ],
+    platforms: {
+      'web/js': {
+        transformGroup: 'tokens-js',
+        buildPath: `dist/web/${brand}/`,
+        files: [
+          {
+            destination: 'tokens.es6.js',
+            format: 'javascript/es6',
+            filter: 'excludeBrand'
+          }
+        ]
+      },
+      'web/json': {
+        transformGroup: 'tokens-json',
+        buildPath: `dist/web/${brand}/`,
+        files: [
+          {
+            destination: 'tokens.json',
+            format: 'json/flat',
+            filter: 'excludeBrand'
+          }
+        ]
+      },
+      'web/scss': {
+        transformGroup: 'tokens-scss',
+        buildPath: `dist/web/${brand}/`,
+        files: [
+          {
+            destination: 'tokens.scss',
+            format: 'scss/variables',
+            filter: 'excludeBrand',
+            options: {
+              outputReferences: false
             }
-        }
-    };
+          }
+        ]
+      }
+    }
+  };
 }
-
 
 /**
  * REGISTER FILTERS
@@ -58,17 +57,17 @@ function getStyleDictionaryConfig(brand, platform) {
 
 // We want to exlude the brand tokens since they're only used to create global tokens
 StyleDictionaryPackage.registerFilter({
-    name: 'excludeBrand',
-    matcher: function (token) {
-        return token.category !== 'brand';
-    }
+  name: 'excludeBrand',
+  matcher: function (token) {
+    return token.category !== 'brand';
+  }
 });
 
 StyleDictionaryPackage.registerFormat({
-    name: 'json/flat',
-    formatter: function (dictionary) {
-        return JSON.stringify(dictionary.allProperties, null, 2);
-    }
+  name: 'json/flat',
+  formatter: function (dictionary) {
+    return JSON.stringify(dictionary.allProperties, null, 2);
+  }
 });
 
 /**
@@ -78,49 +77,49 @@ StyleDictionaryPackage.registerFormat({
  */
 
 StyleDictionaryPackage.registerTransform({
-    name: 'name/prefix/kebab',
-    type: 'name',
-    matcher: function (prop) {
-        return prop.category !== 'alias';
-    },
-    transformer: function (prop) {
-        const prefix = 'token-';
-        return `${prefix}${prop.name}`;
-    }
+  name: 'name/prefix/kebab',
+  type: 'name',
+  matcher: function (prop) {
+    return prop.category !== 'alias';
+  },
+  transformer: function (prop) {
+    const prefix = 'token-';
+    return `${prefix}${prop.name}`;
+  }
 });
 
 StyleDictionaryPackage.registerTransform({
   name: 'name/prefix/constant',
   type: 'name',
   matcher: function (prop) {
-      return prop.category !== 'alias';
+    return prop.category !== 'alias';
   },
   transformer: function (prop) {
-      const prefix = 'TOKEN_';
-      return `${prefix}${prop.name}`;
+    const prefix = 'TOKEN_';
+    return `${prefix}${prop.name}`;
   }
 });
 
 StyleDictionaryPackage.registerTransform({
-    name: 'size/pxToPt',
-    type: 'value',
-    matcher: function (prop) {
-        return prop.value.match(/^[\d.]+px$/);
-    },
-    transformer: function (prop) {
-        return prop.value.replace(/px$/, 'pt');
-    }
+  name: 'size/pxToPt',
+  type: 'value',
+  matcher: function (prop) {
+    return prop.value.match(/^[\d.]+px$/);
+  },
+  transformer: function (prop) {
+    return prop.value.replace(/px$/, 'pt');
+  }
 });
 
 StyleDictionaryPackage.registerTransform({
-    name: 'size/pxToDp',
-    type: 'value',
-    matcher: function (prop) {
-        return prop.value.match(/^[\d.]+px$/);
-    },
-    transformer: function (prop) {
-        return prop.value.replace(/px$/, 'dp');
-    }
+  name: 'size/pxToDp',
+  type: 'value',
+  matcher: function (prop) {
+    return prop.value.match(/^[\d.]+px$/);
+  },
+  transformer: function (prop) {
+    return prop.value.replace(/px$/, 'dp');
+  }
 });
 
 /**
@@ -129,34 +128,29 @@ StyleDictionaryPackage.registerTransform({
  */
 
 StyleDictionaryPackage.registerTransformGroup({
-    name: 'tokens-js',
-    transforms: [
-        'name/cti/constant',
-        'name/prefix/constant',
-        'size/px',
-        'color/hex'
-    ]
+  name: 'tokens-js',
+  transforms: [
+    'name/cti/constant',
+    'name/prefix/constant',
+    'size/px',
+    'color/hex'
+  ]
 });
 
 StyleDictionaryPackage.registerTransformGroup({
-    name: 'tokens-json',
-    transforms: [
-        'attribute/cti',
-        'name/cti/kebab',
-        'size/px',
-        'color/css'
-    ]
+  name: 'tokens-json',
+  transforms: ['attribute/cti', 'name/cti/kebab', 'size/px', 'color/css']
 });
 
 StyleDictionaryPackage.registerTransformGroup({
-    name: 'tokens-scss',
-    transforms: [
-        'name/cti/kebab',
-        'name/prefix/kebab',
-        'time/seconds',
-        'size/px',
-        'color/css'
-    ]
+  name: 'tokens-scss',
+  transforms: [
+    'name/cti/kebab',
+    'name/prefix/kebab',
+    'time/seconds',
+    'size/px',
+    'color/css'
+  ]
 });
 
 /**
@@ -167,22 +161,24 @@ StyleDictionaryPackage.registerTransformGroup({
 console.log('Build started...');
 
 ['web'].map(function (platform) {
-    ['akamai', 'manager'].map(function (brand) {
-        console.log('\n==============================================');
-        console.log(`\nProcessing...\n - Brand: ${brand}\n - Platform: ${platform}`);
+  ['akamai', 'cloudmanager'].map(function (brand) {
+    console.log('\n==============================================');
+    console.log(
+      `\nProcessing...\n - Brand: ${brand}\n - Platform: ${platform}`
+    );
 
-        const StyleDictionary = StyleDictionaryPackage.extend(
-            getStyleDictionaryConfig(brand, platform)
-        );
+    const StyleDictionary = StyleDictionaryPackage.extend(
+      getStyleDictionaryConfig(brand, platform)
+    );
 
-        if (platform === 'web') {
-            StyleDictionary.buildPlatform('web/js');
-            StyleDictionary.buildPlatform('web/json');
-            StyleDictionary.buildPlatform('web/scss');
-        }
+    if (platform === 'web') {
+      StyleDictionary.buildPlatform('web/js');
+      StyleDictionary.buildPlatform('web/json');
+      StyleDictionary.buildPlatform('web/scss');
+    }
 
-        console.log('\nEnd processing');
-    });
+    console.log('\nEnd processing');
+  });
 });
 
 console.log('\n==============================================');
