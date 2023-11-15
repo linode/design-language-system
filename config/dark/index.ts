a4ec7d30f61e462e3365b1fd06fe127ccace0828
Jaalah Ramos <125309814+jaalah-akamai@users.noreply.github.com>
2023-11-15 12:04:14 -0500

    Restructure folder structure to match design requirements (#22)
    Co-authored-by: Jaalah Ramos <jaalah.ramos@gmail.com>

--- config/dark/index.ts
+++ config/dark/index.ts
@@ -1,5 +1,5 @@
-import * as Tokens from '../../dist/cloudmanager/tokens.es6.js';
-import * as TokensTheme from '../../dist/cloudmanager/theme.es6.js';
+import * as Tokens from '../../dist/dark/tokens.es6.js';
+import * as TokensTheme from '../../dist/dark/theme.es6.js';
 import type {
   ColorTypes,
   ElevationTypes,
@@ -10,7 +10,7 @@ import type {
   InteractionTypes,
   ActionTypes,
   ButtonTypes,
-} from '../../dist/cloudmanager/theme.d.ts';
+} from '../../dist/dark/theme.js';
 
 const Colors: ColorTypes = TokensTheme.default.Color;
 const Elevation: ElevationTypes = TokensTheme.default.Elevation;
@@ -20,7 +20,8 @@ const Border: BorderTypes = TokensTheme.default.Border;
 const Background: BackgroundTypes = TokensTheme.default.Background;
 const Interaction: InteractionTypes = TokensTheme.default.Interaction;
 const Actions: ActionTypes = TokensTheme.default.Action;
-const Buttons: ButtonTypes = TokensTheme.default.Button;
+const Button: ButtonTypes = TokensTheme.default.Button;
 
-export { Tokens, Colors, Elevation, Fonts, Content, Border, Background, Interaction, Actions, Buttons };
+export { Tokens, Colors, Elevation, Fonts, Content, Border, Background, Interaction, Actions, Button };
 export type { ColorTypes, ElevationTypes, FontTypes, ContentTypes, BorderTypes, BackgroundTypes, InteractionTypes, ActionTypes, ButtonTypes };
+
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   dts: true,
   entry: [
-    ...['config/akamai/index.ts', 'config/cloudmanager/index.ts'],
-    ...['config/akamai/dark/index.ts', 'config/cloudmanager/dark/index.ts']
+    ...['config/index.ts'],
+    ...['config/dark/index.ts'],
+    ...['config/highContast/index.ts']
   ],
   format: ['esm'],
   minify: 'terser',
